be0c3f717ee6494cc2116cd551d3aa001b16e874
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-09-07 15:46:27 -0300

    Alterado para conectar no banco de dados e salvar o email

--- 08-docker_compose_avancado/backend/app.sh
+++ 08-docker_compose_avancado/backend/app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pip install bottle==0.12.13
+pip install bottle==0.12.18 psycopg2==2.8.6
 python -u sender.py
--- 08-docker_compose_avancado/backend/sender.py
+++ 08-docker_compose_avancado/backend/sender.py
@@ -1,15 +1,40 @@
-from bottle import route, run, request
+import psycopg2
+import os
+import json
+from bottle import Bottle, request
 
 
-@route('/', method='POST')
-def send():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
 
-    return 'Mensagem enfileirada - Assunto: "{}" - Mensagem: "{}"'.format(
-        assunto, mensagem
-    )
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'admin')
+        db_name = os.getenv('DB_NAME', 'email')
+        db_password = os.getenv('DB_PASSWORD', 'pwd')
+        dsn = f'dbname={db_name} user={db_user} host={db_host} password={db_password}'
+        self.conn = psycopg2.connect(dsn)
+
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        print('Mensagem registrada')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada - Assunto: "{}" Mensagem: "{}"'.format(
+            assunto, mensagem
+        )
 
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
